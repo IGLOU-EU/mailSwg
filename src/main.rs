97d71f71572d471478421e1a3ad3815e2c0fb0a3
adrien <adrien@iglou.eu>
2019-03-29 23:13:35 +0100

    rust initial test

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::thread;
+use std::io::{Read,Write,BufReader,BufRead};
+use std::net::TcpListener;
+
+const SERVER: &str = "mailSwg";
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("new client!");
+                println!("connection from {} to {}",
+                         stream.peer_addr().unwrap(),
+                         stream.local_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    let req = &stream.try_clone().unwrap();
+                    let res = stream.try_clone().unwrap();
+                    tcp_send(res, http_json_header("200 OK", "application/json", "{\"success\":true}".len()), "{\"success\":true}");
+                    tcp_read(req);
+                    tcp_read(req);
+                });
+            }
+            Err(e) => println!("Unable to connect: {}", e),
+        }
+    }
+}
+
+fn tcp_send<W>(mut stream: W, header: String, rstr: &str) where W: Write {
+	write!(&mut stream, "{}{}", header, rstr).unwrap();
+}
+
+fn tcp_read<R>(stream: R) where R: Read {
+    let reader = BufReader::new(stream);
+
+    for line in reader.lines() {
+        let lstr = line.unwrap();
+
+        if lstr == "" { break; }
+        println!(">> {}", lstr);
+    }
+}
+
+fn http_json_header(code: &str, ctype: &str, clen: usize) -> String {
+    format!("{}{}{}{}{}{}{}{}{}",
+            "HTTP/1.1", code,
+            "\nserver: ", SERVER,
+            "\ncontent-type: ", ctype,
+            "\nContent-Length: ", clen.to_string(),
+            "\ndomo-arigato: Mr.Roboto\n\n"
+            )
+}
+
+//header("HTTP/1.1 301 Moved Permanently");
+//header("Location: https://example.com/newpage.html");

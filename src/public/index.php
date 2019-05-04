b507f9d0d6cc67b2b98ec5c2595e69076c9b14fb
adrien <adrien@iglou.eu>
2019-05-04 21:38:00 +0200

    Update license

--- src/public/index.php
+++ src/public/index.php
@@ -1,26 +1,8 @@
 <?php
-/* ----------
-* ! NoCopyright, NoCopyleft for a free world !
-* ! PasDeCopyright, PasDeCopyleft pour un monde libre !
-* ----------
-* Copyright (C) [2019] [Kara.Adrien]   <adrien@iglou.eu>
-* ----------
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* ----------
-* http://www.apache.org/licenses/LICENSE-2.0
-* ----------
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* ----------
-* Beta 0.1.0 - 26 mars 2019
-* ----------
-* MailSwg
-** ---------- */
+# Copyright 2019 Iglou.eu
+# Copyright 2019 Adrien Kara
+# V. Beta 0.1.0 - 26 mars 2019
+# license that can be found in the LICENSE file.
 
 define('APP_NAME', 'MailSwg');
 
